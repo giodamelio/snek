9f9dcf9703740fdb2b7efb19cc12d5b063b2c21d
Gio d'Amelio <giodamelio@gmail.com>
2017-01-17 21:30:21 -0800

    Make it go the width of the terminal

--- src/index.js
+++ src/index.js
@@ -7,14 +7,23 @@ const SNEK_CHARS = [
   ['⠁', '⠉'],
 ];
 
-const SNEK_DELAY = 200;
+const SNEK_DELAY = 50;
 
-for (const segment of SNEK_CHARS) {
-  for (const segmentChar of segment) {
-    process.stdout.write(segmentChar);
-    sleep.msleep(SNEK_DELAY);
-    if (segment.indexOf(segmentChar) + 1 !== segment.length) {
-      process.stdout.write('\b');
+// Do one cycle of the snek
+function snekCycle(charset, delay) {
+  for (const segment of charset) {
+    for (const segmentChar of segment) {
+      process.stdout.write(segmentChar);
+      sleep.msleep(delay);
+      if (segment.indexOf(segmentChar) + 1 !== segment.length) {
+        process.stdout.write('\b');
+      }
     }
   }
+}
+
+// Make the snek go all the way across the terminal
+const cycles = Math.floor(process.stdout.columns / SNEK_CHARS.length);
+for (let i = 0; i <= cycles; i++) {
+  snekCycle(SNEK_CHARS, SNEK_DELAY);
 }
