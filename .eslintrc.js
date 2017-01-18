076f4e38e0cd34c106bebab48dd2dcfc26632577
Gio d'Amelio <giodamelio@gmail.com>
2017-01-17 22:15:06 -0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   extends: [
-    '@scottnonnenberg/thehelp',
+    '@scottnonnenberg/thehelp/core',
+    '@scottnonnenberg/thehelp/es2015',
   ],
   rules: {
     'import/no-commonjs': 'off',
+    'arrow-parens': ['error', 'always'],
+    'func-names': 'off',
   },
 };
