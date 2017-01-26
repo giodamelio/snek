eed22f5aecbf3a6912c78ed707f7e3c47afcc18c
Gio d'Amelio <giodamelio@gmail.com>
2017-01-26 14:22:21 -0800

    Basic empty test file

--- test/snek_test.js
+++ test/snek_test.js
@@ -2,15 +2,7 @@ const test = require('ava').test;
 
 const Snek = require('../src');
 
-test.beforeEach((t) => {
-  t.context.outputLog = [];
-  t.context.stream = {
-    write(data) {
-      t.context.outputLog.push(data);
-    },
-  };
-});
-
-test.serial('', (t) => {
+test('Things happen', (t) => {
+  t.pass();
 });
 
